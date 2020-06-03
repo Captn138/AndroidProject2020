389821094282edabee793a952b589c6279008fa6
Mikka138 <mickael.almeida1308@gmail.com>
2020-06-03 19:06:36 +0200

    shared preferences

--- app/src/main/java/com/mobileprogramming/td3/MainActivity.java
+++ app/src/main/java/com/mobileprogramming/td3/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +29,33 @@ public class MainActivity extends AppCompatActivity {
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private static final String BASE_URL = "https://minecraft-ids.grahamedgecombe.com/";
+    private static final String BASE_URL = Constants.getAPI_URL();
+    private SharedPreferences sharedpreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeApiCall();
+        sharedpreferences = getSharedPreferences(Constants.getNAME_SHARED_PREFS(), Context.MODE_PRIVATE);
+        gson = new GsonBuilder().setLenient().create();
+        List<MinecraftItem> list = getDataFromCache();
+        if(list != null) {
+            showList(list);
+        } else {
+            makeApiCall();
+        }
+    }
+
+    private List<MinecraftItem> getDataFromCache() {
+        String stringminecraft = sharedpreferences.getString(Constants.getKEY_MINECRAFT_LIST(), null);
+        if(stringminecraft == null) {
+            return null;
+        } else {
+            Type listType = new TypeToken<List<MinecraftItem>>(){}.getType();
+            return gson.fromJson(stringminecraft, listType);
+        }
     }
 
     private void showList(List<MinecraftItem> list) {
@@ -48,10 +71,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeApiCall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
@@ -66,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
                 if(response.isSuccessful() && response.body() != null){
                     List<MinecraftItem> itemslist = response.body();
                     showList(itemslist);
+                    saveList(itemslist);
                 } else {
                     showError();
                 }
@@ -78,7 +98,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void showError() {
-        Toast.makeText(this, "API Error!", Toast.LENGTH_LONG).show();
+    private void saveList(List<MinecraftItem> itemslist) {
+        String jsonString = gson.toJson(itemslist);
+        sharedpreferences.edit().putString(Constants.getKEY_MINECRAFT_LIST(), jsonString).apply();
+    }
+
+    private void showError() {
+        Toast.makeText(this, Constants.getERROR(), Toast.LENGTH_LONG).show();
     }
 }
